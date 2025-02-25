1386b0406d3057c9d1b54eb9c63b72f0993b0df5
Miles F <miles.fritzmather@gmail.com>
2025-02-25 13:50:34 -0600

    Added sorting functionality to the list. Also added a "fast mode" that speeds up loading from removing images and running status

--- src/open.tsx
+++ src/open.tsx
@@ -3,10 +3,12 @@ import {
   ActionPanel,
   Color,
   getApplications,
+  getPreferenceValues,
   Icon,
   Keyboard,
   List,
   LocalStorage,
+  Application as RayCastApplication,
   showToast,
   Toast,
   useNavigation,
@@ -25,6 +27,12 @@ interface AppPreferences {
   showHidden: boolean;
   quickCommands: Record<string, { modifiers: Keyboard.KeyModifier[]; key: Keyboard.KeyEquivalent }>;
   prioritizeRunningApps: boolean;
+  sortType: "frecency" | "alphabetical" | "custom";
+  appsWithoutRunningCheck: string[];
+}
+
+interface HitHistory {
+  [key: string]: string[];
 }
 
 interface Application {
@@ -49,6 +57,8 @@ function getAppIcon(app: Application): string | Icon {
 }
 
 export default function Command() {
+  const { fastMode, lambdaDecayString } = getPreferenceValues<{ fastMode: boolean; lambdaDecayString: string }>();
+  const lambdaDecay = parseFloat(lambdaDecayString);
   const [applications, setApplications] = useState<Application[]>([]);
   const [preferences, setPreferences] = useState<AppPreferences>({
     hidden: [],
@@ -58,17 +68,21 @@ export default function Command() {
     showHidden: false,
     quickCommands: {},
     prioritizeRunningApps: false,
+    sortType: "frecency",
+    appsWithoutRunningCheck: [],
   });
+  const [hitHistory, setHitHistory] = useState<HitHistory>({});
   const [isLoading, setIsLoading] = useState(true);
+  const [sortType, setSortType] = useState<"frecency" | "alphabetical" | "custom">("frecency");
   const { push } = useNavigation();
 
   useEffect(() => {
-    async function loadPreferences() {
+    async function loadPreferences(): Promise<AppPreferences> {
       try {
         const stored = await LocalStorage.getItem<string>("appPreferences");
         if (stored) {
           const parsedPreferences = JSON.parse(stored);
-          setPreferences({
+          const soonToBePreferences = {
             hidden: parsedPreferences.hidden ?? [],
             customNames: parsedPreferences.customNames ?? {},
             pinnedApps: parsedPreferences.pinnedApps ?? [],
@@ -76,27 +90,70 @@ export default function Command() {
             showHidden: parsedPreferences.showHidden ?? false,
             quickCommands: parsedPreferences.quickCommands ?? {},
             prioritizeRunningApps: parsedPreferences.prioritizeRunningApps ?? true,
-          });
+            sortType: parsedPreferences.sortType ?? "frecency",
+            appsWithoutRunningCheck: parsedPreferences.appsWithoutRunningCheck ?? [],
+          };
+          setPreferences(soonToBePreferences);
+          return soonToBePreferences;
         }
       } catch (error) {
         console.error("Error loading preferences:", error);
       }
+      return {
+        hidden: [],
+        customNames: {},
+        pinnedApps: [],
+        appImportance: {},
+        showHidden: false,
+        quickCommands: {},
+        prioritizeRunningApps: true,
+        sortType: "frecency",
+        appsWithoutRunningCheck: [],
+      };
+    }
+
+    function cleanApplications(
+      apps: RayCastApplication[],
+      runningApps: string[],
+      preferences: AppPreferences,
+    ): Application[] {
+      const specialAppNames: Record<string, string> = {
+        "System Settings": "Settings",
+      };
+
+      const runningCheck = (app: RayCastApplication) => {
+        const name = specialAppNames[app.name] ?? app.name;
+        return runningApps.includes(name) && !preferences.appsWithoutRunningCheck.includes(app.bundleId!);
+      };
+
+      return apps
+        .filter((app) => app && app.bundleId)
+        .map((app) => ({
+          bundleId: app.bundleId!,
+          name: app.name,
+          path: app.path,
+          running: runningCheck(app),
+        }));
     }
 
     async function fetchApplications() {
       try {
-        const apps = await getApplications();
-        const runningApps = await getRunningApps();
-        const cleanedApps = apps
-          .filter((app) => app && app.bundleId)
-          .map((app) => ({
-            bundleId: app.bundleId!,
-            name: app.name,
-            path: app.path,
-            running: runningApps.includes(app.name),
-          }));
-        setApplications(cleanedApps);
-        await loadPreferences();
+        const soonToBePreferences = await loadPreferences();
+        const apps: RayCastApplication[] = await getApplications();
+        if (!fastMode) {
+          const runningApps = await getRunningApps();
+          const cleanedApps = cleanApplications(apps, runningApps, soonToBePreferences);
+          setApplications(cleanedApps);
+        } else {
+          setApplications(
+            apps.map((app) => ({
+              bundleId: app.bundleId!,
+              name: app.name,
+              path: app.path,
+              running: false,
+            })),
+          );
+        }
       } catch (error) {
         console.error("Error fetching applications:", error);
       } finally {
@@ -104,13 +161,20 @@ export default function Command() {
       }
     }
 
-    async function getRunningApps() {
+    async function getRunningApps(): Promise<string[]> {
       try {
         const { stdout } = await execPromise("ps aux | grep -i '.app'");
         const runningApps = stdout
           .split("\n")
-          .filter((line) => line.includes(".app") && line.includes("Applications/"))
-          .map((line) => line.split(/\s+/).pop()?.split("/").pop());
+          .filter(
+            (line) =>
+              line.includes(".app") &&
+              line.includes("Applications/") &&
+              line.split(/\s+/).pop() !== undefined &&
+              line.split(/\s+/).pop()!.split("/").pop() !== undefined,
+          )
+          .map((line) => line.split(/\s+/).pop()!.split("/").pop()!)
+          .filter((app) => !app!.startsWith("--"));
 
         return runningApps;
       } catch (error) {
@@ -119,7 +183,19 @@ export default function Command() {
       }
     }
 
-    getRunningApps();
+    async function fetchHitHistory() {
+      try {
+        const stored = await LocalStorage.getItem<string>("hitHistory");
+        if (stored) {
+          const parsedHitHistory = JSON.parse(stored);
+          setHitHistory(parsedHitHistory);
+        }
+      } catch (error) {
+        console.error("Error loading hit history:", error);
+      }
+    }
+
+    fetchHitHistory();
     fetchApplications();
   }, []);
 
@@ -133,24 +209,74 @@ export default function Command() {
         .filter((app) => preferences.showHidden && preferences.hidden.includes(app.bundleId))
         .sort(sortFunction),
     ];
-  }, [applications, preferences]);
+  }, [applications, preferences, sortType]);
+
+  function frecency(appId: string) {
+    const now = new Date();
+    return (
+      hitHistory[appId]?.reduce((total, timestamp) => {
+        return (
+          // Measured in 6 hours increments
+          total + Math.exp(-lambdaDecay * ((now.getTime() - new Date(timestamp).getTime()) / 3600000 / 6))
+        );
+      }, 0) ?? 0
+    );
+  }
 
   function sortFunction(a: Application, b: Application) {
     if (preferences.prioritizeRunningApps) {
       if (a.running && !b.running) return -1;
       if (!a.running && b.running) return 1;
     }
-    const aImportance = preferences.appImportance[a.bundleId ?? ""] ?? 0;
-    const bImportance = preferences.appImportance[b.bundleId ?? ""] ?? 0;
-    return bImportance - aImportance;
+
+    switch (sortType) {
+      case "frecency": {
+        return frecency(b.bundleId) - frecency(a.bundleId);
+      }
+      case "alphabetical":
+        return a.name.localeCompare(b.name);
+      case "custom":
+        return a.name.localeCompare(b.name);
+    }
   }
 
-  async function toggle(type: "pinnedApps" | "hidden", bundleId: string) {
+  async function toggle(
+    type: "pinnedApps" | "hidden" | "appsWithoutRunningCheck" | "prioritizeRunningApps" | "showHidden",
+    bundleId: string,
+  ) {
     const newPreferences = { ...preferences };
-    if (newPreferences[type].includes(bundleId)) {
-      newPreferences[type] = newPreferences[type].filter((id) => id !== bundleId);
-    } else {
-      newPreferences[type].push(bundleId);
+    switch (type) {
+      case "pinnedApps":
+        if (newPreferences.pinnedApps.includes(bundleId)) {
+          newPreferences.pinnedApps = newPreferences.pinnedApps.filter((id) => id !== bundleId);
+        } else {
+          newPreferences.pinnedApps.push(bundleId);
+        }
+        break;
+      case "hidden":
+        if (newPreferences.hidden.includes(bundleId)) {
+          newPreferences.hidden = newPreferences.hidden.filter((id) => id !== bundleId);
+        } else {
+          newPreferences.hidden.push(bundleId);
+        }
+        break;
+      case "appsWithoutRunningCheck":
+        if (newPreferences.appsWithoutRunningCheck.includes(bundleId)) {
+          newPreferences.appsWithoutRunningCheck = newPreferences.appsWithoutRunningCheck.filter(
+            (id) => id !== bundleId,
+          );
+          updateRunningStatus(applications.find((app) => app.bundleId === bundleId)!, true, true);
+        } else {
+          newPreferences.appsWithoutRunningCheck.push(bundleId);
+          updateRunningStatus(applications.find((app) => app.bundleId === bundleId)!, false, true);
+        }
+        break;
+      case "prioritizeRunningApps":
+        newPreferences.prioritizeRunningApps = !newPreferences.prioritizeRunningApps;
+        break;
+      case "showHidden":
+        newPreferences.showHidden = !newPreferences.showHidden;
+        break;
     }
     setPreferences(newPreferences);
     await LocalStorage.setItem("appPreferences", JSON.stringify(newPreferences));
@@ -166,10 +292,10 @@ export default function Command() {
     await LocalStorage.setItem("appPreferences", JSON.stringify(newPreferences));
   }
 
-  function updateRunningStatus(app: Application, running: boolean) {
+  function updateRunningStatus(app: Application, running: boolean, force: boolean = false) {
     const newApplications: Application[] = applications.map((a) => {
       if (a.bundleId === app.bundleId) {
-        return { ...a, running: running };
+        return { ...a, running: running && (force || !preferences.appsWithoutRunningCheck.includes(a.bundleId)) };
       }
       return a;
     });
@@ -181,7 +307,7 @@ export default function Command() {
     incrementImportance(app);
   }
 
-  async function onAppClose(app: Application) {
+  async function closeApp(app: Application) {
     updateRunningStatus(app, false);
     try {
       setIsLoading(true);
@@ -197,27 +323,23 @@ export default function Command() {
   }
 
   function incrementImportance(app: Application) {
-    const newPreferences = { ...preferences };
-    newPreferences.appImportance[app.bundleId ?? ""] = (newPreferences.appImportance[app.bundleId ?? ""] ?? 0) + 1;
-    setPreferences(newPreferences);
-    LocalStorage.setItem("appPreferences", JSON.stringify(newPreferences));
+    const newHitHistory = { ...hitHistory };
+    newHitHistory[app.bundleId] = (newHitHistory[app.bundleId] ?? []).concat([new Date().toISOString()]);
+    setHitHistory(newHitHistory);
+    LocalStorage.setItem("hitHistory", JSON.stringify(newHitHistory));
   }
 
   const AppItem = ({ preferences, app }: { preferences: AppPreferences; app: Application }) => {
     return (
       <List.Item
         key={app.bundleId}
-        icon={getAppIcon(app)}
+        icon={!fastMode ? getAppIcon(app) : undefined}
         title={preferences.customNames[app.bundleId] || app.name}
         subtitle={preferences.customNames[app.bundleId] ? app.name : ""}
         accessories={[
           {
             icon: app.running ? { source: Icon.Bolt, tintColor: Color.Green } : undefined,
             tooltip: app.running ? "Running" : "Not Running",
-          },
-          {
-            icon: preferences.pinnedApps.includes(app.bundleId) ? Icon.Tack : undefined,
-            tooltip: "Pinned",
           },
         ]}
         actions={
@@ -232,7 +354,7 @@ export default function Command() {
               <Action
                 title="Close Application"
                 icon={Icon.XMarkCircle}
-                onAction={() => onAppClose(app)}
+                onAction={() => closeApp(app)}
                 shortcut={{ modifiers: ["ctrl"], key: "x" }}
               />
             )}
@@ -264,21 +386,29 @@ export default function Command() {
                 }
                 shortcut={{ modifiers: ["cmd", "shift"], key: "n" }}
               />
+              <Action
+                title={
+                  preferences.appsWithoutRunningCheck.includes(app.bundleId)
+                    ? "Check Running Status"
+                    : "Don't Check Running Status"
+                }
+                icon={preferences.appsWithoutRunningCheck.includes(app.bundleId) ? Icon.Bolt : Icon.BoltDisabled}
+                onAction={() => toggle("appsWithoutRunningCheck", app.bundleId)}
+                shortcut={{ modifiers: ["cmd", "shift"], key: "d" }}
+              />
             </ActionPanel.Section>
             <ActionPanel.Section title={"General View"}>
               <Action
                 title={preferences.showHidden ? "Hide All Hidden Apps" : "Show All Hidden Apps"}
                 icon={preferences.showHidden ? Icon.CircleDisabled : Icon.Circle}
-                onAction={() => setPreferences({ ...preferences, showHidden: !preferences.showHidden })}
+                onAction={() => toggle("showHidden", app.bundleId)}
               />
               <Action
                 title={
                   preferences.prioritizeRunningApps ? "Ignore Running Status When Sorting" : "Pull Running Apps to Top"
                 }
                 icon={preferences.prioritizeRunningApps ? Icon.BoltDisabled : Icon.Bolt}
-                onAction={() =>
-                  setPreferences({ ...preferences, prioritizeRunningApps: !preferences.prioritizeRunningApps })
-                }
+                onAction={() => toggle("prioritizeRunningApps", app.bundleId)}
               />
             </ActionPanel.Section>
           </ActionPanel>
@@ -293,6 +423,20 @@ export default function Command() {
       filtering={{
         keepSectionOrder: true,
       }}
+      searchBarAccessory={
+        <List.Dropdown
+          tooltip="Sort by"
+          onChange={(value) => {
+            const newSortType = value as "frecency" | "alphabetical" | "custom";
+            setSortType(newSortType);
+            LocalStorage.setItem("sortType", newSortType);
+          }}
+        >
+          <List.Dropdown.Item title="Frecency" value="frecency" icon={Icon.Clock} />
+          <List.Dropdown.Item title="Alphabetical" value="alphabetical" icon={Icon.Text} />
+          <List.Dropdown.Item title="Custom" value="custom" icon={Icon.Pencil} />
+        </List.Dropdown>
+      }
     >
       <List.Section title="Pinned Apps">
         {pinnedApps.map((app) => (
